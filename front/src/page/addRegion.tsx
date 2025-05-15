ada6edc05436d074998fa60be21a5d02388e97f8
Azamat Suleimenov <aza517@mail.ru>
2025-05-15 16:30:49 +0500

    post работает

--- front/src/page/addRegion.tsx
+++ front/src/page/addRegion.tsx
@@ -1,13 +1,62 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const AddRegion = () => {
+
+    const [image, setImage] = useState('')
+    const [id_stops, setId_stops] = useState('')
+
+
+
+
+    const postData = async () => {
+        if (!image || !id_stops
+        ) {
+            alert('Введите данные');
+            return;
+        }
+        const formData = new FormData();
+
+        formData.append('image', image,);
+        formData.append('id_stops', id_stops,);
+
+        try {
+            const response = await fetch(`http://localhost:4500/region`, {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            console.log('Success:', result);
+            return true
+        } catch (error) {
+            console.error('Error posting data:', error);
+        }
+    };
+
+    const fileChangeHandler = (e: any) => {
+        if (e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
+    const navigate = useNavigate();
+    function save() {
+        postData()
+        navigate('/');
+    }
+
+
     return (
         <div className="addRegion">
             <div className="addRegion__box">
                 <div className="addRegion__box__label">Остановки по области</div>
                 <div className="addRegion__box__id">
                     <div className="addRegion__box__id__box">ID Остановки</div>
-                    <input placeholder="id" className="addRegion__box__id__inp" type="text" />
+                    <input value={id_stops} onChange={(e) => setId_stops(e.target.value)} placeholder="id" className="addRegion__box__id__inp" type="text" />
                 </div>
                 <div className="addRegion__box__id">
                     <div className="addRegion__box__id__box">Наименование на русском</div>
@@ -33,7 +82,13 @@ const AddRegion = () => {
                     <div className="addRegion__box__id__box">Улицы</div>
                     <input placeholder="Улицы" className="addRegion__box__id__inp" type="text" />
                 </div>
-                <div className="addRegion__box__id__box">
+                <div className="addRegion__box__id">
+                    <div className="addRegion__box__id__box">Улицы</div>
+                    <input name="image" onChange={fileChangeHandler} type="file" />
+                </div>
+
+                <div className="addRegion__box__id">
+                    <div className="addRegion__box__id__box">Районы</div>
                     <select className="addRegion__select">
                         <option>Районы</option>
                         <option>Алмалинский</option>
@@ -44,8 +99,9 @@ const AddRegion = () => {
                         <option>Турксибский</option>
                     </select>
                 </div>
+
                 <div className="addRegion__btn">
-                    <button className="addRegion__btn__box ">Add</button>
+                    <button onClick={save} className="addRegion__btn__box ">Add</button>
                 </div>
             </div>
         </div>
